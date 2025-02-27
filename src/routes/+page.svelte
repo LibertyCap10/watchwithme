e55a15e89b709c478e57ec8b9daebecbf2300aa1
LibertyCap10 <60331991+LibertyCap10@users.noreply.github.com>
2025-02-26 22:13:53 -0600

    remove empty anchor elements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,14 +16,14 @@
     <header class="absolute inset-x-0 top-0 z-50">
       <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div class="flex lg:flex-1">
-          <a href="#" class="-m-1.5 p-1.5">
+          <!-- <a href="#" class="-m-1.5 p-1.5">
             <span class="sr-only">Your Company</span>
-            <!-- <img
+            <img
               class="h-8 w-auto"
               src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
               alt=""
-            /> -->
-          </a>
+            />
+          </a> -->
         </div>
         <div class="flex lg:hidden">
           <button
@@ -50,10 +50,10 @@
           </button>
         </div>
         <div class="hidden lg:flex lg:gap-x-12">
+          <!-- <a href="#" class="text-sm/6 font-semibold text-gray-900"></a>
           <a href="#" class="text-sm/6 font-semibold text-gray-900"></a>
           <a href="#" class="text-sm/6 font-semibold text-gray-900"></a>
-          <a href="#" class="text-sm/6 font-semibold text-gray-900"></a>
-          <a href="#" class="text-sm/6 font-semibold text-gray-900"></a>
+          <a href="#" class="text-sm/6 font-semibold text-gray-900"></a> -->
         </div>
         <!-- <div class="hidden lg:flex lg:flex-1 lg:justify-end">
           <a href="#" class="text-sm/6 font-semibold text-gray-900"
@@ -68,14 +68,14 @@
             class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
           >
             <div class="flex items-center justify-between">
-              <a href="#" class="-m-1.5 p-1.5">
+              <!-- <a href="#" class="-m-1.5 p-1.5">
                 <span class="sr-only">Your Company</span>
                 <img
                   class="h-8 w-auto"
                   src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
                   alt=""
                 />
-              </a>
+              </a> -->
               <button
                 type="button"
                 class="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -97,7 +97,7 @@
             </div>
             <div class="mt-6 flow-root">
               <div class="-my-6 divide-y divide-gray-500/10">
-                <div class="space-y-2 py-6">
+                <!-- <div class="space-y-2 py-6">
                   <a
                     href="#"
                     class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
@@ -125,7 +125,7 @@
                     class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                     >Log in</a
                   >
-                </div>
+                </div> -->
               </div>
             </div>
           </div>
